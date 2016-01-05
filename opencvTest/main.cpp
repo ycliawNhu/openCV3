91727a27a27db8a95c8fb3311a158cc7b681bff1
ycliaw <ycliaw@mail.nhu.edu.tw>
2016-01-05 22:25:36 +0800

    Initial Commit

---
+++ opencvTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvTest
+//
+//  Created by ycliaw on 2016/1/5.
+//  Copyright © 2016年 ___NHU___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
